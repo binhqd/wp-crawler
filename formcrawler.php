995379af7dec9f472aff431babaf2e30e82878e0
Binh Quan <binhqd@gmail.com>
2015-02-04 00:23:03 +0700

    update changes

--- formcrawler.php
+++ formcrawler.php
@@ -3,11 +3,11 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Crawler images</title>
-	<script src="/js/jquery.min.js"></script>
+	<script src="./js/jquery.min.js"></script>
 </head>
 <body>
 	<form action="crawlerform.php" method="POST">
-		<textarea id="listPhotos" name="listPhotos"></textarea>
+		<textarea id="listPhotos" name="listPhotos" rows='6' cols='60'></textarea><br/>
 		<input type="submit" name="submit" id="upload"/>
 	</form>
 </body>
